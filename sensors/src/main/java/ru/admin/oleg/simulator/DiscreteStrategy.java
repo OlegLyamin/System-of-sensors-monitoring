3fa54c74a23cd8b790c5ae3651a35e674e9e2fab
OlegLyamin <mr.lyamin@inbox.ru>
2018-10-11 19:48:00 +0700

    create ApplicationConfig.xml for generate informations about sensors with beans

---
+++ sensors/src/main/java/ru/admin/oleg/simulator/DiscreteStrategy.java
@@ -0,0 +1,41 @@
+package ru.admin.oleg.simulator;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DiscreteStrategy implements SimulatorStrategy {
+    private static Logger Log = LoggerFactory.getLogger(DiscreteStrategy.class);
+    private int counter;
+    private int value;
+    private int pulse;
+
+    public DiscreteStrategy() {
+        Log.info("СОЗДАНИЕ ДИСКРЕТНОЙ СТРАТЕГИИ ИЗ БИНА");
+        this.value = 0;
+        this.pulse = 1;
+    }
+    @Override
+    public void doSimulate(SensorSetter sensor) {
+        if(this.pulse % this.counter == 0)
+        {
+            if(this.value == 0)
+            {
+                this.value = 1;
+            }else
+            {
+                this.value = 0;
+            }
+            this.pulse = 1;
+        }else
+        {
+            this.pulse++;
+        }
+        sensor.setValue(value);
+
+    }
+
+    @Override
+    public void setPulseCounter(int counter) {
+
+    }
+}
